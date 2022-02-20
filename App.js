061f7a19a0232469dd00d8b5efcceef3a9738478
Melike Görgülü <45533057+melikeg@users.noreply.github.com>
2022-02-20 14:27:51 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import * as React from "react";
 import { LoginPage } from "./src/pages/LoginPage";
-import { RegisterPage } from "./src/pages/RegisterPage";
+import { RegisterPage } from "./src/pages/RegisterPage"; 
 import { ForgotPasswordPage } from "./src/pages/ForgotPasswordPage";
 import { Main } from "./src/pages/Main";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; //Gerekli modüller eklenir.
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LoginPage" component={LoginPage} />
+      <Stack.Navigator> //Stack Navigotor ekranlar arası geçişi sağlar.
+        <Stack.Screen name="LoginPage" component={LoginPage} /> //Stack.Screen ile navigator'de olmasını istediğimiz ekranları belirleriz.
         <Stack.Screen name="RegisterPage" component={RegisterPage} />
         <Stack.Screen
           name="ForgotPasswordPage"
